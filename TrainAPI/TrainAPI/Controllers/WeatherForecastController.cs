5ded6f324e49da942a9becad261b8b5643ed1ec9
Guy Lau <guylau@microsoft.com>
2022-01-10 15:19:12 -0500

    test

--- TrainAPI/TrainAPI/Controllers/WeatherForecastController.cs
+++ TrainAPI/TrainAPI/Controllers/WeatherForecastController.cs
@@ -26,6 +26,7 @@ namespace TrainAPI.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            // comment
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
